df3a9fd20465ab5839a8a767cd74171f8d3da34f
nimeom <nimeom@hotmail.com>
2018-02-14 14:03:09 +0100

    LoginPage, InstanceLocator

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+
+namespace Lands
+{
+    using Xamarin.Forms;
+    using Views;
+
+    public partial class App : Application
+	{
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+    
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+    #endregion
+
+    #region Contructors
+
+    public InstanceLocator()
+    {
+            this.Main = new MainViewModel();
+
+    }
+    #endregion
+}
+}
